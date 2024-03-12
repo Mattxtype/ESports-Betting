6a7695d7f25e6b4769e95c3ebbf27f45d71f9bdb
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-12 14:01:46 -0700

    create user model for auth service

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { User } from "../models/user";
 
 const router = express.Router();
 
-router.post("/api/auth/signup", (req, res) => {
+router.post("/api/auth/signup", async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  console.log(email, password);
+  //(TODO) check if error with fields
+
+  //(TODO) check if email is already registered
+
+  const user = User.build({email, password});
+  await user.save();
+
+  console.log(User.find());
 
   res.status(201).send({ email, password });
 });
