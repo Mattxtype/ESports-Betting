942cf1bb958889165f41c7bd14b328aeec510669
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-12 16:08:39 -0700

    Dockerize auth, add deployment files for auth and its corresponding mongodb instance, and use skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { json } from "body-parser";
 import { signupRouter } from "./routes/signup";
 
@@ -7,6 +8,17 @@ app.use(json());
 
 app.use(signupRouter);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth"); //Auth is the database name
+    console.log("connected to mongodb")
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("listening on port 3000");
+  });
+};
+
+start();
