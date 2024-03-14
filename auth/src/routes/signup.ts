929b99005a6c7952c12e958c1a308b1b4d98845a
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-14 16:49:30 -0700

    Use express-validator to check that email and password meet requirements, throw error if they do not

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,18 +1,34 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 import { User } from "../models/user";
 
 const router = express.Router();
 
-router.post("/api/auth/signup", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  //(TODO) check if error with fields
+router.post(
+  "/api/auth/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"), //If no email or wrong email then return this message
+    body("password")
+      .trim() //eliminate leading and trailing 0's
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be betwveen 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      //(TODO) Print actually errors out in custom error class
+      throw new Error();
+    }
+  
+    const { email, password } = req.body;
 
-  //(TODO) check if email is already registered
+    //(TODO) check if email is already registered
 
-  const user = User.build({email, password});
-  await user.save();
+    const user = User.build({ email, password });
+    await user.save();
 
-  res.status(201).send({ email, password });
-});
+    res.status(201).send({ email, password });
+  }
+);
 
 export { router as signupRouter };
