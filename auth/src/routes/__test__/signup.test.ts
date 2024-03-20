a791f5568556e1d5310bb9229fd23058e2afec83
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-20 11:00:26 -0700

    testing implementation

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on success signup', async () => {
+    return request(app)
+        .post("/api/auth/signup")
+        .send({
+            email: "goodEmail@gmail.com",
+            password: "goodPassword"
+        })
+        .expect(201);
+});
