4e0127ebb1f0afb690abe374694a0c0e648e50a2
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-21 17:01:36 -0700

    return JWT when user successfully signs in

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { validateRequest } from "../middleware/validate-request";
 import { User } from "../models/user";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -21,7 +23,25 @@ async (req: Request, res: Response) => {
     const existingUser = await User.findOne({email});
     if(!existingUser) {
         throw new Error("Login request failed");
+    } 
+    const passwordMatch = await Password.compare(existingUser.password, password);
+    if (!passwordMatch) {
+        throw new Error("invalid credentials");
     }
+
+     //generate jwt
+     const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+      }, 
+      process.env.JWT_KEY!);
+  
+      //store it on session object
+      req.session = {
+        jwt: userJwt
+      };
+
+      res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
