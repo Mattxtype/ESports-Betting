6a2afb0d6c3d8afdc08ffa17b348fa0ad1200ace
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-22 09:37:56 -0700

    turn current user authentication into a middleware, that will be shared in the future with other microservices

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,11 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import jwt from "jsonwebtoken";
+import { currentUser } from "../middleware/current-user";
 
 const router = express.Router();
 
-router.get('/api/auth/currentuser', async (req, res) => {
-    if(!req.session?.jwt) {
-        return res.send({ currentUser: null })
-    }
+router.get("/api/auth/currentuser", currentUser, (req, res) => {
+    res.send({currentUser: req.currentUser})
+});
 
-    try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({currentUser: payload})
-    } catch (err) {
-        res.send({ currentUser: null })
-    }
-})
-
-export {router as currentUserRouter}
+export { router as currentUserRouter };
