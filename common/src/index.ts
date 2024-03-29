46117eb11fc21ba342e31c5dc63d93f3c6f46bc5
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-03-29 11:15:29 -0700

    export error functions for npm module

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,3 +3,7 @@ export * from './middleware/require-auth';
 export * from './middleware/validate-request';
 
 export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/request-validation-error';
