cc0ef774dcaab546923923b0e2911a0f39592033
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-04-02 12:49:47 -0700

    Nav Bar initial implentation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
-import { SignupCard } from "@/components/custom/signup_card";
+import { SignupCard } from "@/components/auth/signup_card";
+import { NavBar } from "@/components/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <NavBar></NavBar>
       <div>
       <SignupCard/>
       </div>
