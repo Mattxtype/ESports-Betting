ea1bd3c3c16c33a3d3cea53e6e391ef570c81bc1
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-04-02 23:21:28 -0700

    impelemnt signin page with correct routing

---
+++ client/app/auth/signin/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { FormEvent, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { NavBar } from "@/components/navbar/navbar";
+
+async function onSubmit(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+  var formData = new FormData(event.currentTarget);
+  const form_values = Object.fromEntries(formData);
+  console.log("form values", form_values);
+  await axios
+    .post("/api/auth/signin", {
+      email: form_values.email,
+      password: form_values.password,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+export default function SignIn() {
+  return (
+    <div>
+    <NavBar></NavBar>
+      <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign In
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={onSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+          <div>
+            <Link href={"/auth/signup"}>Create Account</Link>
+          </div>
+        </div>
+      </div>
+      </div>
+  );
+};
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,10 @@
-import { SignupCard } from "@/components/auth/signup_card";
 import { NavBar } from "@/components/navbar/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <NavBar></NavBar>
       <div>
-      <SignupCard/>
       </div>
     </main>
   );
